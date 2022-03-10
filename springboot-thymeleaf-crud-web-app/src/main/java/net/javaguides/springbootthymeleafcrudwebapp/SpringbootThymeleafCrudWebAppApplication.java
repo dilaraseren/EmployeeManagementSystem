cb4acaace5851f441e74bcef522ac938d015030a
dilara153232@gmail.com <dilara153232@gmail.com>
2022-03-10 15:47:34 +0300

    Initial commit

---
+++ springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/springbootthymeleafcrudwebapp/SpringbootThymeleafCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootthymeleafcrudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ springboot-thymeleaf-crud-web-app/src/test/java/net/javaguides/springbootthymeleafcrudwebapp/SpringbootThymeleafCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootthymeleafcrudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
